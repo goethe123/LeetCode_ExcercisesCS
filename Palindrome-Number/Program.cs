7e4013a960d12e60f4d426d89f6b906e7907275f
goethe123 <goethe123123@gmail.com>
2025-09-14 22:00:12 -0600

    feat palindrome excersise

---
+++ Palindrome-Number/Program.cs
@@ -0,0 +1,39 @@
+﻿// Given an integer x, return true if x is a palindrome, and false otherwise.
+using System;
+using System.Collections.Generic;
+
+
+namespace Palindrome
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string Numero = "121";
+
+            if (PalindromeValidation(Numero))
+            {
+                System.Console.WriteLine($"the number {Numero} is in fact a palindrome");
+            }
+            else
+            {
+                 System.Console.WriteLine($"the number {Numero} is in Not a palindrome");
+            }
+
+        }
+
+        public static bool PalindromeValidation(string Numero)
+        {
+           
+            char[] caracteres = Numero.ToCharArray();
+            char[] reverso = caracteres.Reverse().ToArray();
+
+            return caracteres.SequenceEqual(reverso);
+
+        }
+
+
+    }
+}
+
