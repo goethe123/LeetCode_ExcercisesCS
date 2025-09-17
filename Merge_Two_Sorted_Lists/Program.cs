2ebc681289f82360ce2f280cfca7520ed921f062
goethe123 <goethe123123@gmail.com>
2025-09-17 11:19:47 -0600

    feat Find Array

---
+++ Merge_Two_Sorted_Lists/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Merge_Two_Sorted_Lists
+{
+    public class program
+    {
+        public static void Mian(string[] args)
+        {
+            int[] List1 = new int[] { 1, 2, 4 };
+            int[] List2 = new int[] { 1, 3, 4 };
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int Val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new ListNode();  
+            ListNode current = dummy;         
+
+            while (list1 != null && list2 != null) 
+            {
+                if (list1.val <= list2.val)        
+                {
+                    current.next = list1;         
+                    list1 = list1.next;           
+                }
+                else
+                {
+                    current.next = list2;         
+                    list2 = list2.next;           
+                }
+                current = current.next;           
+            }
+
+            current.next = (list1 != null) ? list1 : list2; 
+
+            return dummy.next; 
+        }
+
+    }
+}
---
+++ Unsupported_Type/Program.cs
@@ -0,0 +1,57 @@
+﻿// You have a list of numbers and a single number. You need to check if the single number is in the list.
+//  If it is, you say 'Yes', and if it isn't, you say 'No'.
+// 5
+// 3 1 4 1 5
+// 4
+// Output:
+// Yes
+
+using System;
+using System.Collections.Generic;
+
+namespace Hamana
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 1, 4, 1, 5 };
+            System.Console.WriteLine("Tu array es \n");
+            foreach (int i in array)
+            {
+                System.Console.Write(i + ",");
+            }
+            System.Console.WriteLine("intro duce tu target");
+            int Target = Convert.ToInt32(Console.ReadLine());
+
+            bool finded = Validate(array, Target);
+    
+            if (finded)
+            {
+                System.Console.WriteLine("Yes, esta en la posicion");
+            }
+            else
+            {
+                System.Console.WriteLine("No");
+            }
+          
+            
+
+        }
+
+        public static bool Validate(int[] array, int Target)
+        {
+            
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == Target)
+                {
+              
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
