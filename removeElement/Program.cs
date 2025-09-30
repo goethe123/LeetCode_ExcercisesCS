4ac9b0558eda06c74d5b2186c47c06fb6ad792eb
goethe123 <goethe123123@gmail.com>
2025-09-30 17:55:19 -0600

    removeElement

---
+++ removeElement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace removeElement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numeros = [3, 2, 2, 3];
+            int val = 3;
+
+            System.Console.WriteLine(Solution.RemoveElement(numeros, val));
+        }
+    }
+}
